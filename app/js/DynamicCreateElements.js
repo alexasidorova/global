952f38854e99519ef9a61824e404bb545bd79586
alexasidorova <shurka.rms@yandex.ru>
2018-11-27 07:34:58 +0700

    task3 not completed()

--- app/js/DynamicCreateElements.js
+++ app/js/DynamicCreateElements.js
@@ -8,34 +8,37 @@ mainCentralBlockListPartImg.className = "main_central-block_list_part-img";
       var aplications = [{image: "assets/img-1.png", 
                         title:'СТАНДАРТНЫЙ ПАКЕТ', 
                         date:'08 апреля 2012' },
+                        
                         {image: "assets/img-2.png", 
                         title:'НОВЫЙ ЦФТ-БАНК', 
                         date:'09 сентября 2016'},
+                       
                         {image: "assets/img-3.png", 
                         title:'КАТАЛОГ РАЗРАБОТОК', 
-                        date:'03 марта 2015'}];
+                        date:'03 марта 2015'},
+                       ];
       function apps () {
  for (i=0;i<aplications.length;i++){
 
-    let j = Math.floor(Math.random() * aplications.length);
+ //   let j = Math.floor(Math.random() * aplications.length);
 
     var mainCentralBlockListPartImgWrapper1 = document.createElement('div');
     mainCentralBlockListPartImgWrapper1.className = "main_central-block_list_part-img_wrapper1";  
     var mainCentralBlockListPartImg1 = document.createElement('figure');
     mainCentralBlockListPartImg1.className = "main_central-block_list_part-img-1";
     var img1 = document.createElement('img');
-    img1.src = aplications[j].image;
+    img1.src = aplications[i].image;
     img1.className = "main_central-block_list_part-image";
    
 
     var captionBig1 = document.createElement('figcaption');
     captionBig1.className = "caption-big1";
-    var textBig1 = document.createTextNode(aplications[j].title);
+    var textBig1 = document.createTextNode(aplications[i].title);
    
 
     var captionLittle1 = document.createElement('figcaption');
     captionLittle1.className = "caption-little1";
-    var textLittle1 = document.createTextNode(aplications[j].date);
+    var textLittle1 = document.createTextNode(aplications[i].date);
    
 
     
@@ -56,3 +59,26 @@ mainCentralBlockListPartImg.className = "main_central-block_list_part-img";
  }
 };
  apps();
+let k = 1;
+//  function shiftRight()
+// {
+//     for(var j = 0; j < k; j ++)
+//     {
+//         var tmp = aplications[aplications.length - 1];
+ 
+//         for (var i = aplications.length - 1; i > 0; i --)
+//         {
+//          aplications[i] = aplications[i - 1];
+//         }
+ 
+//         aplications[0] = tmp;
+//     }
+// }
+function shiftArrayToRight(aplications, k) {
+   for (var i = 0; i < k; i++) {
+       aplications.unshift(aplications.pop());
+   }
+}
+ var mainCentralBlockListPartImgPost = document.querySelector('div.main_central-block_list_part-img_post');
+ mainCentralBlockListPartImgPost.addEventListener("click",shiftRight);
+ 
